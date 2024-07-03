56bd47c98684446720e298600b56b907293796c4
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2024-07-03 01:27:46 -0300

    Update streamlit_app_estudo.py

--- streamlit_app_estudo.py
+++ streamlit_app_estudo.py
@@ -16,17 +16,18 @@ if st.button('resetar'):
 if 'contador' not in st.session_state:
     st.session_state['contador'] = 0
 
-# Função para incrementar o contador de 1 em 1 até 60, intervalado em 1s
-def incrementar_contador():
+# Função para incrementar o contador de 1 em 1 até 60, intervalado de acordo com `seconds`
+def incrementar_contador(seconds):
     placeholder = st.empty()
+    interval = seconds / 60  # Intervalo de tempo em segundos para cada incremento
     for i in range(st.session_state['contador'], 61):
         st.session_state['contador'] = i
         placeholder.write(f"vc clicou esse tanto {st.session_state['contador']}")
-        time.sleep(60/(seconds*100))
+        time.sleep(interval)
         # Utilize o placeholder para evitar st.experimental_rerun()
         if st.session_state['contador'] >= 60:
             break
 
 # Botão para iniciar o incremento do contador
 if st.button("clica"):
-    incrementar_contador()
+    incrementar_contador(seconds)
