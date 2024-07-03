c15d81cc646f04429a17aedd6b892ea22244f7af
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2024-07-03 01:08:21 -0300

    Update streamlit_app_estudo.py

--- streamlit_app_estudo.py
+++ streamlit_app_estudo.py
@@ -4,3 +4,9 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.write('testeeeee')
+
+st.button("Reset", type="primary")
+if st.button("se gostou da um joinhao"):
+    st.write("ai sim")
+else:
+    st.write("Goodbye")
