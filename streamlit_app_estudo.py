0da50871480f086533dce64712b1a249f5bbe4b0
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2024-07-03 01:19:45 -0300

    Update streamlit_app_estudo.py

--- streamlit_app_estudo.py
+++ streamlit_app_estudo.py
@@ -6,28 +6,23 @@ st.set_page_config(layout="wide")
 
 st.write('testeeeee')
 
-# Inicializar o contador e o estado de execução no estado do Streamlit
+# Inicializar o contador no estado do Streamlit
 if 'contador' not in st.session_state:
     st.session_state['contador'] = 0
-if 'executando' not in st.session_state:
-    st.session_state['executando'] = False
 
-# Função para incrementar o contador de 1 em 1 até 60, intervalado em 0.1s
+# Função para incrementar o contador de 1 em 1 até 60, intervalado em 1s
 def incrementar_contador():
     placeholder = st.empty()
-    while st.session_state['executando'] and st.session_state['contador'] < 60:
-        st.session_state['contador'] += 1
+    for i in range(st.session_state['contador'], 61):
+        st.session_state['contador'] = i
         placeholder.write(f"vc clicou esse tanto {st.session_state['contador']}")
-        time.sleep(0.5)
-        st.experimental_rerun()  # Recarregar a página para atualizar a interface
+        time.sleep(0.2)
+        # Utilize o placeholder para evitar st.experimental_rerun()
+        if st.session_state['contador'] >= 60:
+            break
 
 # Botão para iniciar o incremento do contador
 if st.button("clica"):
-    st.session_state['executando'] = True
     incrementar_contador()
-
-# Botão para parar o incremento do contador
-if st.button("para"):
-    st.session_state['executando'] = False
 
 st.write(f"vc clicou esse tanto {st.session_state['contador']}")
