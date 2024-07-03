8ce43283a11080ac2ee2d32240bf3251d30bc851
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2024-07-03 01:20:28 -0300

    Update streamlit_app_estudo.py

--- streamlit_app_estudo.py
+++ streamlit_app_estudo.py
@@ -16,7 +16,7 @@ def incrementar_contador():
     for i in range(st.session_state['contador'], 61):
         st.session_state['contador'] = i
         placeholder.write(f"vc clicou esse tanto {st.session_state['contador']}")
-        time.sleep(0.1)
+        time.sleep(0.2)
         # Utilize o placeholder para evitar st.experimental_rerun()
         if st.session_state['contador'] >= 60:
             break
